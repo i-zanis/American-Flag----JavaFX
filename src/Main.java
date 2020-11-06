907f582c2340fb5178c5cad8296461ece6d5c911
i-zanis <ioanlafazanis@gmail.com>
2020-11-06 01:26:43 +0000

    01261106

--- src/Main.java
+++ src/Main.java
@@ -15,23 +15,11 @@ import javafx.util.Duration;
  * American Flag Graphic designed with basic geometry and horizontal animation created in JavaFX.
  * Horizontal animation on mouse press.
  * Pauses on mouse-dragging
- *
- *  * Name:          Ioannis Lafazanis
- *  * Student ID:    21425229
- *  * Course Code:   CP40061E - Programming
- *  * Date:          18/10/2020
- *  * Project Name:  Practical Activity 1
- *  * Module Leader: Dr Ikram Ur Rehman
- *
- * Last year TKinter was presented in the lecture and I was overwhelmed.
- * This year I looked back to the slides and I smiled.
- * I will try to make up for what I did not do.
- *
+
  * Made on OpenJDK14.
  * Please download "javafx-sdk-11.0" and add all the jar-files in
  * Javafx-sdk-11.0.2\lib\ to the global library.
  * VM options --module-path [%java path%] --add-modules javafx.controls,javafx.fxml
- * Thank you for your patience.
  */
 
 public class Main extends Application {
